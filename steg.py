cfa14531ad63b34042284dc5f53f824399de4b20
Gobisan <gobi.s2011@vit.ac.in>
2015-11-05 23:43:32 +0530

    Update steg.py

--- steg.py
+++ steg.py
@@ -9,21 +9,14 @@ def enc():
  curl=raw_input("Enter destination of the crypted image :")
  im2.save(curl,"PNG")
  print('Image has been saved\n\nDo you want to encrypt in another image')
- ei=input('Press \n1-Yes\n0-No\n')
- if(int(ei) is 1):
-  enc()
- elif(int(ei) is 0):
-  print('Thaank you')
+ 
 
 def dec():
  image=raw_input("enter url of image :")
  im=Image.open(image)
  data=stepic.decode(im)
  print data
- di=input('Do you want to decrypt any othr image?\nPress \n1-Yes\nAny key to exit-No\n')
- if(int(di) is 1):
-  dec()
- else :
+ 
   print('Thaank you')
 
 s=raw_input('enter \n1-encryption\n2-decryption\n\n')
